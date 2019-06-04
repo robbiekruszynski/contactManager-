ae1fd48efa5fd12bd36a3da79e6ce71eca966b25
Robbie Kruszynski <robbiekruszynski@gmail.com>
2019-06-04 06:26:44 -0700

    add components and basic work with props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>The App Component</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header branding="Contact Manager" />
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
+        <Contact
+          name="Karen Smith"
+          email="karen@gmail.com"
+          phone="555-444-4444"
+        />
+      </div>
+    );
+  }
 }
-
 export default App;
